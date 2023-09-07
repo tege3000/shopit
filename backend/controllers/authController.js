b3854dbe3242d5aa15658efa8375673ef7b5acc1
Tito Egeonu <teetoegeonu@gmail.com>
2023-09-07 09:24:06 +0100

    Add Logout User, Authorize User Roles and Permissions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-
+const sendToken = require('../utils/jwtToken');
 
 // Register a user    => /api/v1/register
 exports.registerUser = catchAsyncErrors( async (req, res, next) => {
@@ -19,12 +19,7 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 })
 
 // Login User  => /api/v1/login
@@ -49,11 +44,19 @@ exports.loginUser = catchAsyncErrors(async(req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
+    sendToken(user, 200, res)
+})
+
+
+// Logout user    => /api/v1/logout
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
 
     res.status(200).json({
         success: true,
-        token
+        message: 'Logged out'
     })
-
 })
