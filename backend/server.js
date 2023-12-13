842e3541533ea1cd9ca0e9bb89fafb3905cea194
Tito Egeonu <teetoegeonu@gmail.com>
2023-12-13 23:15:19 +0100

    Add Delete Order and other Order Resources

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const server = app.listen(process.env.PORT, () => {
 
 // Handle Unhandled Promise rejections
 process.on(`unhandledRejection`, err => {
-    console.log(`ERROR: ${err.message}`);
+    console.log(`ERROR: ${err.stack}`);
     console.log(`Shutting down the server due to Unandled Promise rejection`);
     server.close(() => {
         process.exit(1)
